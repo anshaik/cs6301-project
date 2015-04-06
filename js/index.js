07329edad14a20188f57cf17ef37a4e8e6a70eb2
SneakyNachos <sneakynachos@gmail.com>
2015-04-06 14:18:27 -0500

    Small modifications to index

---
+++ js/index.js
@@ -0,0 +1,6 @@
+$("document").ready(function(){
+	$("#search").click(function(){
+		//Debug check
+		console.log("Search has been pushed");
+	});
+});
--- php/signin.php
+++ php/signin.php
@@ -1,7 +1,9 @@
-/*
+
+<?php
+	/*
 	this page will signin user and create a session 
 	two levels of signin: normal user and admin user
-*/
-<?php
-
+	*/
+	
+	
 ?>
