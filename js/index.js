bebcbb82c19240e6346b163d189dd64dabaf8b39
SneakyNachos <sneakynachos@gmail.com>
2015-04-06 15:23:38 -0500

    Setup Sign In to set state variable for logging in, Sign In functionality implemented.

--- js/index.js
+++ js/index.js
@@ -1,6 +1,63 @@
+//Clean the errors from the alert div
+function clearErrors(){
+	$(".alert-area").empty();
+}
+//Cleans up print success
+function printSuccess($alert,$id_name){
+	$(".alert-area").append("<div class=\"alert alert-success\" role=\"alert\">"+$id_name+" "+$alert+"</div>");
+}
+
+//Cleans up printing alert a billion times
+function printCritical($alert,$id_name){
+	$(".alert-area").append("<div class=\"alert alert-danger\" role=\"alert\">"+$id_name+" "+$alert+"</div>");
+}
+
+function login($email,$password){
+	//Ajax call to query to Login
+	return $.ajax({
+		url: "php/signin.php",
+		dataType: "text",
+		type: "POST",
+		data: {
+			email : email,
+			password : password
+		}
+	});
+}
+
+
 $("document").ready(function(){
 	$("#search").click(function(){
 		//Debug check
 		console.log("Search has been pushed");
+	});
+	$("#signinbutton").click(function(){
+		
+		//Cleanup
+		clearErrors();
+		
+		//Check if already Logged in
+		if(localStorage['loggedin'] == 'yes'){
+			printCritical('Your already logged in!','');
+		}else {
+			//Get Email and Password
+			email = $("#email").val();
+			password = $("#password").val();
+
+			//Login
+			login(email,password).done(function(r){
+				if(r == 1){
+					//Success, Set Session variables
+					printSuccess('Login Successful!','');
+					localStorage['loggedin'] = "yes";
+				} 
+				else {
+					//Fail, print failure
+					printCritical('Username or password is incorrect','');
+				}
+			});
+		
+		}
+		
 	});
 });
--- php/signin.php
+++ php/signin.php
@@ -1,9 +1,35 @@
 
 <?php
 	/*
-	this page will signin user and create a session 
-	two levels of signin: normal user and admin user
+		this page will signin user and create a session 
+		two levels of signin: normal user and admin user
 	*/
+	//Setup connection
+	$conn = new mysqli('localhost','buser','buser','mydb');
+	
+	//Check if connected
+	if($conn->connect_error){
+		die('Could not connect: '.$conn->connect_error);
+	}
+	
+	//Get all the variables from the POST request
+	$email = $conn->real_escape_string($_POST['email']);
+	$password = $conn->real_escape_string($_POST['password']);
+	
+	//Create Query
+	$sql = "SELECT email, password FROM user WHERE email = '$email' AND password='$password'";
+	
+	//Check if query was successful
+	if($conn->query($sql)){
+		//Success
+		echo 1;
+	} else {
+		//Fail
+		echo 0;
+	}
+	
+	//End connection
+	$conn->close();
 	
 	
 ?>
