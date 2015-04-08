5601316e0111716a42d73267f96cfd8dd55df681
SneakyNachos <sneakynachos@gmail.com>
2015-04-08 11:06:23 -0500

    Changed search box layout on index.html

---
+++ js/search.js
@@ -0,0 +1,10 @@
+function searchFlights($source,$destination,$date,$returndate){
+	
+}
+$("document").ready(function(){
+	//Search button was clicked
+	$("search").click(function(){
+		//Debug statement
+		console.log("Search was clicked!");
+	});
+});
